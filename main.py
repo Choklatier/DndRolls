7833ef8b106b37cd9e23b5c8a7ab6b723f082b59
thmaurin <t.maurin.1@research.gla.ac.uk>
2024-03-17 16:28:54 +0000

    Updated plotting

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ players  = ["Lucas"]
 for playername in players:
     playerRolls = Rolls(f"data/{playername}.txt",playername)
     for session in sessions:
-        fig,axes = plt.subplots(ncols = 3)
+        fig,axes = plt.subplots(ncols = 3,figsize = (16,10))
         playerRolls.plotRolls(session,axis = axes[0])
         playerRolls.plotCumul(session,axis = axes[1])
         playerRolls.plotSumm (session,axis = axes[2])
         fig.suptitle(f"{playername}'s rolling performance \n for session {session}",
                      fontsize = 16)
+
         plt.tight_layout()
         plt.savefig(f"pngs/{playername}.png")
         plt.show()
