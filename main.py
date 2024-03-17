c240a1ad2f8ed57928d1ad740001bac3ba47f64c
thmaurin <t.maurin.1@research.gla.ac.uk>
2024-03-17 16:25:38 +0000

    Updated plotting

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ for playername in players:
         playerRolls.plotRolls(session,axis = axes[0])
         playerRolls.plotCumul(session,axis = axes[1])
         playerRolls.plotSumm (session,axis = axes[2])
-        fig.suptitle(f"{playername}'s rolling performance \n for session {session}")
+        fig.suptitle(f"{playername}'s rolling performance \n for session {session}",
+                     fontsize = 16)
+        plt.tight_layout()
         plt.savefig(f"pngs/{playername}.png")
         plt.show()
 
